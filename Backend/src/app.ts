adbf7bf317417e23a942dcb4990c3e75617efd42
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-05-02 16:40:05 +0900

    setting for project
    setting for project

---
+++ Backend/src/app.ts
@@ -0,0 +1,10 @@
+import express, {Request, Response, NextFunction} from 'express';
+import index from './router/index';
+
+const app = express();
+const port = 3002;
+
+app.use('/',index);
+
+const hostname = "10.120.75.224"
+app.listen(port,hostname, () => console.log(`listening on port ${port}!`));
