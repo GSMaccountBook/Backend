6260ca6319f53106f32b85a65938e5acefa0b5bd
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-07-21 18:35:30 +0900

    update dashboard
    update dashboard

--- nest-backend/src/app.service.ts
+++ nest-backend/src/app.service.ts
@@ -5,4 +5,12 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getMoney():any {
+    let data = {
+      money:50000,
+      spend:12000,
+      TodaySpend:1100
+    }
+    return data;
+  }
 }
