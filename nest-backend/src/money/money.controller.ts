c6160ff09a11d8040183e9b85acb0e7e0f527b9b
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-08-03 19:58:03 +0900

    bring jwt
    bring jwt

--- nest-backend/src/money/money.controller.ts
+++ nest-backend/src/money/money.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post,Body, Get } from '@nestjs/common';
+import { Controller, Post,Body, Get, Headers } from '@nestjs/common';
+import jwtDecode from 'jwt-decode';
 import { ICreateMoneydata } from 'src/moneydata/moneydata.interface';
 import { MoneydataService } from 'src/moneydata/moneydata.service';
 
@@ -7,11 +8,18 @@ export class MoneyController {
     constructor(private readonly moneydataservice: MoneydataService) {}
 
     @Post()
+    async asdf(@Body() update:ICreateMoneydata, @Headers() access) {
+        console.log(update);
+        
+    }
+
+    @Post('create')
     async findAll(@Body() createmoneyDto:ICreateMoneydata) {
         const money_result = await this.moneydataservice.createMoneydata(createmoneyDto);
         console.log(money_result)
         return "변경되었습니다."
     }
+
     @Get()
     async getData() {
         let data = await this.moneydataservice.getData()
