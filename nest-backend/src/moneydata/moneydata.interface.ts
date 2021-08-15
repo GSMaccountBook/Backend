3e0a1fd24f3fe796900dc8507fc81bfe90aa6cd0
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-08-15 19:37:15 +0900

    update interface for swagger
    update interface for swagger

--- nest-backend/src/moneydata/moneydata.interface.ts
+++ nest-backend/src/moneydata/moneydata.interface.ts
@@ -1,6 +1,12 @@
-export interface ICreateMoneydata {
+import { ApiProperty } from "@nestjs/swagger"
+
+export class ICreateMoneydata {
+    @ApiProperty({description:'user id'})
     userid:string
+    @ApiProperty({description:'보유 돈'})
     money:number
+    @ApiProperty({description:"지출"})
     spend:number
+    @ApiProperty({description:"수입"})
     income:number
 }
