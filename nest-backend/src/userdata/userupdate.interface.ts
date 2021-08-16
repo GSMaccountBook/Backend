7a69a987b1adc4a775a5b90cb800abbd25cb5f38
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-08-16 21:51:17 +0900

    update user interface
    update user interface

--- nest-backend/src/userdata/userupdate.interface.ts
+++ nest-backend/src/userdata/userupdate.interface.ts
@@ -3,6 +3,8 @@ import { ApiProperty } from "@nestjs/swagger"
 export class IUpdateUserdata {
     @ApiProperty({description:'username'})
     username:string;
+    @ApiProperty({description:'user email'})
     email:string;
+    @ApiProperty({description:'user id'})
     userid:string;
 }
