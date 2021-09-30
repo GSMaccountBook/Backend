4e9f132f5ceca31949cb4ba80f659d36f4bff94e
정인교 <69563418+ingyocode@users.noreply.github.com>
2021-09-30 11:29:10 +0900

    chore: Delete past folder
    chore: Delete past folder

--- Backend/src/router/index.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response, NextFunction } from "express";
-
-const index = express.Router();
-
-index.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("world");
-});
-
-export = index;
