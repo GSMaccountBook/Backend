f51bbad8eaaa954437e101e3afe66879fca8e3ac
정인교 <69563418+ingyocode@users.noreply.github.com>
2021-09-30 11:28:03 +0900

    chore: Delete unneccessary import
    chore: Delete unneccessary import

--- nest-backend/src/app.module.ts
+++ nest-backend/src/app.module.ts
@@ -1,26 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DashboardController } from './dashboard/dashboard.controller';
-import { DashboardService } from './dashboard/dashboard.service';
 import { DashboardModule } from './dashboard/dashboard.module';
-import { LoginController } from './login/login.controller';
 import { LoginModule } from './login/login.module';
 import { UserdataModule } from 'src/userdata/userdata.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/userdata/user.entity';
-import { UserdataService } from 'src/userdata/userdata.service';  
 import { RegisterModule } from './register/register.module';
 import { AuthModule } from './auth/auth.module';
-import { RegisterController } from './register/register.controller';
-import { MoneydataService } from './money/entity/money.service';
 import { MoneydataModule } from './money/entity/money.module';
 import { Money } from './money/entity/money.entity';
-import { MoneyController } from './money/money.controller';
-import { FindPwController } from './find-pw/find-pw.controller';
-import { LoginService } from './login/login.service';
 import { FindPwModule } from './find-pw/find-pw.module';
-import { MypageController } from './mypage/mypage.controller';
 import { MypageModule } from './mypage/mypage.module';
 import { ConfigModule } from '@nestjs/config';
 
